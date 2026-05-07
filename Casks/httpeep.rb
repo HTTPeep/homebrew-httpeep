c0d5cb281e0215acc713f0ccacddb028c029ac41
ImChris <imchrisorz@gmail.com>
2026-05-07 12:09:38 +0800

    Fix HTTPeep cask audit

--- Casks/httpeep.rb
+++ Casks/httpeep.rb
@@ -5,8 +5,7 @@ cask "httpeep" do
   sha256 arm:   "c90867bead1438ff82686e7f15dcc08bb9977ac7977882c3e915f7d65f5ad4f3",
          intel: "14a2a51e50cf44dd1e1f7d0c97e3ba690e1db16b2b0d6ab15f557f8a61516292"
 
-  url "https://s1.httpeep.com/v#{version}/HTTPeep_#{version}_#{arch}.dmg",
-      verified: "s1.httpeep.com/"
+  url "https://s1.httpeep.com/v#{version}/HTTPeep_#{version}_#{arch}.dmg"
   name "HTTPeep"
   desc "HTTP and HTTPS proxy debugging tool"
   homepage "https://httpeep.com/"
